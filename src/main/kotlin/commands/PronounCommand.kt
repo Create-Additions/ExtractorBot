91146f0ffdbb239184366a9252980a7755c09bd5
kotakotik22 <>
2021-07-15 13:56:43 +0300

    pronouns remove command

--- src/main/kotlin/commands/PronounCommand.kt
+++ src/main/kotlin/commands/PronounCommand.kt
@@ -10,9 +10,11 @@ import org.javacord.api.interaction.*
 @Subscribe
 class PronounCommand : HandyCommand() {
     override fun register(): SlashCommandBuilder {
-        return SlashCommandBuilder().setName("pronouns").setDescription("Add pronouns")
-            .addOption(SlashCommandOption.createWithChoices(SlashCommandOptionType.STRING, "pronouns", "What pronoun do you want to add?", true,
-            getPronouns()))
+        return SlashCommandBuilder().setName("pronouns").setDescription("Add or remove pronouns")
+            .addOption(SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "add", "Add pronouns",
+                listOf(SlashCommandOption.createWithChoices(SlashCommandOptionType.STRING, "name","The pronouns to add", true, getPronouns()))))
+            .addOption(SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "remove", "Remove pronouns",
+                listOf(SlashCommandOption.createWithChoices(SlashCommandOptionType.STRING, "name","The pronouns to remove", true, getPronouns()))))
     }
 
     fun getPronouns(): List<SlashCommandOptionChoice> {
@@ -27,9 +29,14 @@ class PronounCommand : HandyCommand() {
 
     override fun onCalled(ctx: SlashCommandInteraction) {
         val p = getPronounRoles()
-        val chosenRole = ctx.firstOption.get().stringValue.get()
+        val chosenRole = ctx.firstOption.get().firstOption.get().stringValue.get()
         val role = p.find { it.id.toString() == chosenRole }!!
-        ctx.user.addRole(role)
-        simpleUserOnlyResponse(ctx, "Role ${role.mentionTag} given")
+        val add = ctx.firstOption.get().equals("add")
+        if(add) {
+            ctx.user.addRole(role)
+        } else {
+            ctx.user.removeRole(role)
+        }
+        simpleUserOnlyResponse(ctx, "Role ${role.mentionTag} ${if(add) "given" else "removed"}")
     }
 }
