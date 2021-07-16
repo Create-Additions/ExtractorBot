70286bc84d80e975489bef8dce1bb1a733e7ab5f
kotakotik22 <>
2021-07-16 12:10:28 +0300

    funny button is for cool ppl only

--- src/main/kotlin/handy/commands/HahaFunnyButtonCommand.kt
+++ src/main/kotlin/handy/commands/HahaFunnyButtonCommand.kt
@@ -1,12 +1,11 @@
 package handy.commands
 
-import handy.HandyDiscord.api
 import handy.base.HandyCommand
 import handy.base.Subscribe
-import org.javacord.api.entity.message.Message
 import org.javacord.api.entity.message.component.ActionRow
 import org.javacord.api.entity.message.component.Button
-import org.javacord.api.entity.user.User
+import org.javacord.api.entity.permission.PermissionState
+import org.javacord.api.entity.permission.PermissionType
 import org.javacord.api.interaction.SlashCommandBuilder
 import org.javacord.api.interaction.SlashCommandInteraction
 import org.javacord.api.interaction.SlashCommandOption
@@ -19,9 +18,11 @@ class HahaFunnyButtonCommand : HandyCommand() {
 
     override fun register(): SlashCommandBuilder {
         return builder("haha", "fun").addOption(
-            SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND_GROUP, "funny", "fun",
-            listOf(SlashCommandOption.create(SlashCommandOptionType.SUB_COMMAND, "button", "yes"))
-            ))
+           SlashCommandOption.createWithOptions(
+               SlashCommandOptionType.SUB_COMMAND_GROUP, "funny", "fun",
+               listOf(SlashCommandOption.create(SlashCommandOptionType.SUB_COMMAND, "button", "yes"))
+           )
+        )
     }
 
     val button = addComponentSubscriber { event, interaction ->
@@ -32,10 +33,14 @@ class HahaFunnyButtonCommand : HandyCommand() {
     }
 
     override fun onCalled(ctx: SlashCommandInteraction) {
-        simpleResponse(ctx, getText("nobody", "0")) {
-            it.addComponents(ActionRow.of(
-                Button.primary(button, "haha funny button")
-            ))
+        if(ctx.user.getRoles(ctx.server.get()).any {it.permissions.getState(PermissionType.MANAGE_CHANNELS).equals(PermissionState.ALLOWED)}) {
+            simpleResponse(ctx, getText("nobody", "0")) {
+                it.addComponents(ActionRow.of(
+                    Button.primary(button, "haha funny button")
+                ))
+            }
+        } else {
+            simpleUserOnlyResponse(ctx, "Only people with permissions to manage channels can send the funny button")
         }
     }
 }
