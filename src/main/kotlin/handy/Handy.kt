81ad6f17a46a9fde038a64bec5561a26ffe52712
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-12-10 20:34:08 +0300

    lmao thanks log4j

--- src/main/kotlin/handy/Handy.kt
+++ src/main/kotlin/handy/Handy.kt
@@ -6,7 +6,8 @@ object Handy {
     val json = Json {  prettyPrint = true; encodeDefaults = true }
 
     @JvmStatic
-        fun main(args: Array<String>) {
+    fun main(args: Array<String>) {
+            System.setProperty("log4j2.formatMsgNoLookups", "true");
             HandyDiscord.create()
     }
 }
